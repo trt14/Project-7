beba090eed0fee9e22742d944687430df9f60112
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-09-14 19:13:13 +0300

    Debug: add screend for debug

---
+++ lib/screen_debug/api_test.dart
@@ -0,0 +1,14 @@
+// this screen only for debug after the project is complete delete this file
+import 'package:flutter/material.dart';
+
+class ApiTest extends StatelessWidget {
+  const ApiTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: 
+      SafeArea(child: Text("This a debug screen")),
+    );
+  }
+}
