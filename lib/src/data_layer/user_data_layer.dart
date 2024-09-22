290a21dc6262a18aafbfc3869ffaa0ea8b718a00
FaHaDoF69 <93477293+FaHaDoF69@users.noreply.github.com>
2024-09-22 19:13:09 +0300

    Logic: Save all data using SharedPreferences

--- lib/src/data_layer/user_data_layer.dart
+++ lib/src/data_layer/user_data_layer.dart
@@ -1,18 +1,125 @@
-import 'package:intl/intl.dart';
+import 'dart:convert';
+
+import 'package:get_it/get_it.dart';
 import 'package:project_7/src/models/auth/auth_model.dart';
 import 'package:project_7/src/models/project/project_model.dart';
 import 'package:project_7/src/models/user/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserDataLayer {
   UserModel? user;
+
   AuthModel? auth;
   List<ProjectModel>? projects = [];
   String? email;
 
-  logoutFunction() {
+  // Initialize SharedPreferences
+  final SharedPreferences prefs = GetIt.I<SharedPreferences>();
+
+  // Constructor to initialize the UserDataLayer
+  UserDataLayer() {
+    // Load data from SharedPreferences when the UserDataLayer is instantiated
+    loadData();
+  }
+
+  /*
+  *
+  * Method: Load authentication data from SharedPreferences
+  *
+  * */
+  Future<void> loadData() async {
+    // Retrieve the 'auth' data from SharedPreferences as a JSON string
+    final String? authJson = prefs.getString('auth');
+    if (authJson != null) {
+      auth = AuthModel.fromJson(jsonDecode(authJson));
+    }
+
+    // Retrieve the 'user' data from SharedPreferences as a JSON string
+    final String? userJson = prefs.getString('user');
+    if (userJson != null) {
+      user = UserModel.fromJson(jsonDecode(userJson));
+    }
+
+    // Retrieve the 'projects' data from SharedPreferences as a JSON string
+    final String? projectsJson = prefs.getString('projects');
+    if (projectsJson != null) {
+      projects = (jsonDecode(projectsJson) as List)
+          .map((item) => ProjectModel.fromJson(item))
+          .toList();
+    }
+
+    // Retrieve the 'email' data from SharedPreferences as a string
+    email = prefs.getString('email');
+  }
+
+  /*
+  *
+  * Method: Save user data to SharedPreferences
+  *
+  * */
+  Future<void> saveUser(UserModel user) async {
+    this.user = user;
+    await prefs.setString('user', jsonEncode(user.toJson()));
+  }
+
+  /*
+  *
+  * Method: Save authentication data to SharedPreferences
+  *
+  * */
+  Future<void> saveAuth(AuthModel auth) async {
+    this.auth = auth;
+    await prefs.setString('auth', jsonEncode(auth.toJson()));
+  }
+
+  /*
+  *
+  * Method: Save projects data to SharedPreferences
+  *
+  * */
+  Future<void> saveProjects(List<ProjectModel> projects) async {
+    this.projects = projects;
+    await prefs.setString(
+        'projects', jsonEncode(projects.map((item) => item.toJson()).toList()));
+  }
+
+  /*
+  *
+  * Method: Save all data to SharedPreferences
+  *
+  * */
+  Future<void> saveData() async {
+    if (auth != null) {
+      await prefs.setString('auth', jsonEncode(auth!.toJson()));
+    }
+
+    if (user != null) {
+      await prefs.setString('user', jsonEncode(user!.toJson()));
+    }
+
+    if (projects != null) {
+      await prefs.setString('projects',
+          jsonEncode(projects!.map((item) => item.toJson()).toList()));
+    }
+
+    if (email != null) {
+      await prefs.setString('email', email!);
+    }
+  }
+
+  /*
+  *
+  * Method: Logout function to clear all user data and save to SharedPreferences
+  *
+  * */
+  Future<void> logoutFunction() async {
     user = null;
     auth = null;
     projects?.clear();
     email = null;
+    await prefs.remove('user');
+    await prefs.remove('auth');
+    await prefs.remove('projects');
+    await prefs.remove('email');
   }
 }
--- lib/src/helper/setup.dart
+++ lib/src/helper/setup.dart
@@ -1,9 +1,13 @@
 import 'package:get_it/get_it.dart';
 import 'package:project_7/src/data_layer/project_data_layer.dart';
 import 'package:project_7/src/data_layer/user_data_layer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> setup() async {
   GetIt.I.registerSingleton<UserDataLayer>(UserDataLayer());
   GetIt.I.registerSingleton<ProjectDataLayer>((ProjectDataLayer()));
 
+  // Shared Preferences setup
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  GetIt.I.registerSingleton<SharedPreferences>(sharedPreferences);
 }
